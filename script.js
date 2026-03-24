be22a0fcc2c2e9bc88dd49a65a0edbe087a43222
Jeevantha Perera <jeevanthaa99@gmail.com>
2026-03-24 23:28:37 +0530

    Navbar tutorial, just for fun hahahah :)

---
+++ script.js
@@ -0,0 +1,13 @@
+// const toggle = document.querySelector(".Menu");
+// const navlinks = document.querySelector(".links");
+
+// toggle.addEventListener("click", () => {
+//     navlinks.classList.toggle("active");
+// });
+
+const toggle = document.querySelector(".menu");
+const navlinks = document.querySelector(".nav-links");
+
+toggle.addEventListener("click", ()=>{
+    navlinks.classList.toggle("active");
+});
